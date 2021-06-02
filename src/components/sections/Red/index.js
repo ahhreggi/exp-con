d8c309763eccf104dc04f1671472a6eb605ccd66
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-02 16:19:25 -0400

    Fix Red section padding

--- src/components/sections/Red/index.js
+++ src/components/sections/Red/index.js
@@ -1,28 +1,32 @@
 import PropTypes from "prop-types";
 import Button from "../../Button";
+import Nav from "../../Nav";
 import speaker from "../../../images/speaker.png";
 import "./index.scss";
 
 const Red = (props) => {
   Red.propTypes = {
-
+    setView: PropTypes.func
   };
   return (
     <div className="Red">
+      <Nav showButton={true} styles="red" setView={props.setView} />
       <main>
-        <h1>SUPERIOR SOUND</h1>
-        <h2>Experience live versions of your favourite songs.</h2>
-        <Button text="SEE DEMO" styles="large red" />
+        <section className="left">
+          <h1>SUPERIOR SOUND</h1>
+          <h2>Experience live versions of your favourite songs.</h2>
+          <Button text="SEE DEMO" styles="large red" />
+        </section>
+        <section className="right">
+          <div className="speaker">
+            <img src={speaker} />
+          </div>
+          <div className="speaker">
+            <img src={speaker} />
+          </div>
+          <Button text="CLICK" styles="circle" />
+        </section>
       </main>
-      <section className="interactive">
-        <div className="speaker">
-          <img src={speaker} />
-        </div>
-        <div className="speaker">
-          <img src={speaker} />
-        </div>
-        <Button text="CLICK" styles="circle" />
-      </section>
     </div>
   );
 };
