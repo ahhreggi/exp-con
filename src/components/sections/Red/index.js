9f849935563cd226f2e33757961b81e9ad24c894
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-02 15:33:45 -0400

    Add speaker images, Red section styles

--- src/components/sections/Red/index.js
+++ src/components/sections/Red/index.js
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import Button from "../../Button";
+import speaker from "../../../images/speaker.png";
+import "./index.scss";
 
 const Red = (props) => {
   Red.propTypes = {
@@ -7,9 +9,20 @@ const Red = (props) => {
   };
   return (
     <div className="Red">
-      <h1>SUPERIOR SOUND</h1>
-      <h2>Experience live versions of your favourite songs.</h2>
-      <Button text="SEE DEMO" styles="large red" />
+      <main>
+        <h1>SUPERIOR SOUND</h1>
+        <h2>Experience live versions of your favourite songs.</h2>
+        <Button text="SEE DEMO" styles="large red" />
+      </main>
+      <section className="interactive">
+        <div className="speaker">
+          <img src={speaker} />
+        </div>
+        <div className="speaker">
+          <img src={speaker} />
+        </div>
+        <Button text="CLICK" styles="circle" />
+      </section>
     </div>
   );
 };
