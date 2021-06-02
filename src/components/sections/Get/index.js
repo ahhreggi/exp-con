36700d6918bee4e46c093e20ff703a3de32278de
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-02 18:46:08 -0400

    Add Get component

---
+++ src/components/sections/Get/index.js
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import "./index.scss";
+import Nav from "../../Nav";
+import Button from "../../Button";
+
+const Get = (props) => {
+
+  Get.propTypes = {
+    setView: PropTypes.func
+  };
+
+  return (
+    <div className="Get">
+
+      {/* Nav Bar */}
+      <Nav showButton={true} styles="bg-rainbow" setView={props.setView} />
+
+      {/* Main Content */}
+      <main>
+
+        {/* Section Text */}
+        <div className="left">
+          <h1>GET EXP|CON NOW</h1>
+          <h2>Purchase and download the app.</h2>
+        </div>
+
+        {/* Try It Now Button */}
+        <div className="right">
+          <Button text="TRY IT NOW" styles="bg-rainbow" />
+        </div>
+
+      </main>
+
+    </div>
+  );
+};
+
+export default Perks;
