7a2fed329cd215b7a0dc65ea8b2039a30fe99346
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-02 19:02:16 -0400

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import "./Footer.scss";
+
+const Footer = () => {
+
+  Footer.propTypes = {
+    setView: PropTypes.func
+  };
+
+  return (
+    <div className="Footer">
+
+      <div className="contact">
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Get;
