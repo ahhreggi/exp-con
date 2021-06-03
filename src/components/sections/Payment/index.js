c0dc1202819e5d1778001aee5d1a18f65936ee2d
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-03 01:59:17 -0400

    Add Payment form containers

--- src/components/sections/Payment/index.js
+++ src/components/sections/Payment/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./index.scss";
 import Nav from "../../Nav";
+import Button from "../../Button";
 
 const Payment = (props) => {
 
@@ -19,7 +20,36 @@ const Payment = (props) => {
       <main>
 
         {/* Section Header */}
-        <div className="text">PAYMENT</div>
+        <h1>PAYMENT</h1>
+
+        <div className="step-one">
+          <h3>1. Select your plan</h3>
+          <div className="plans">
+
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="step-two">
+            <h3>2. Billing Information</h3>
+            <div className="fields">
+
+            </div>
+          </div>
+          <div className="step-three">
+            <h3>3. Credit Card Information</h3>
+            <div className="fields">
+
+            </div>
+          </div>
+        </div>
+
+        <div className="submit">
+          <div>
+            By continuing, I acknowledge that I&apos;ve read and agree to the <a href="">Terms of Service</a> & <a href="">Privacy Policy</a>.
+          </div>
+          <Button text="DOWNLOAD" styles="bg-rainbow" />
+        </div>
 
       </main>
 
