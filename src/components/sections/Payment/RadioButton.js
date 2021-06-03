ab49bb72e5edacad1d23c81df875a33a857e709e
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-03 04:39:32 -0400

    Add comments

--- src/components/sections/Payment/RadioButton.js
+++ src/components/sections/Payment/RadioButton.js
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types";
 
 const RadioButton = (props) => {
+
   RadioButton.propTypes = {
     label: PropTypes.string,
     selected: PropTypes.bool,
     styles: PropTypes.string,
     onClick: PropTypes.func
   };
+
   return (
     <div className={`RadioButton ${props.styles}`} onClick={() => props.onClick(props.label)}>
+
+      {/* Toggler */}
       <div className={`toggle ${props.selected ? "selected" : ""}`} />
+
+      {/* Label */}
       <span>{props.label}</span>
+
     </div>
   );
 };
