0d3049da5d4e7a4ebc34ca968acdb27c42e461a8
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-03 00:01:20 -0400

    Add Payment and form components

---
+++ src/components/sections/Payment/index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./index.scss";
+import Nav from "./Nav";
+
+const Payment = (props) => {
+
+  Payment.propTypes = {
+    setView: PropTypes.func
+  };
+
+  return (
+    <div className="Payment">
+
+      {/* Nav Bar */}
+      <Nav setView={props.setView} section="info" />
+
+      {/* Main Content */}
+      <main>
+
+      </main>
+
+    </div>
+  )
+}
+
+export default Payment
