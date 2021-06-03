54c8baf7363e641250bf125182966e82eb372d97
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-06-03 14:31:58 -0400

    Update yellow section

--- src/components/sections/Yellow/index.js
+++ src/components/sections/Yellow/index.js
@@ -14,8 +14,6 @@ const Yellow = (props) => {
     showImage1: false
   });
 
-  const image = "https://images.unsplash.com/photo-1598518142096-254d1d4d34a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80";
-
   const showImage = (image) => {
     const key = "showImage" + image;
     setState({ ...state, [key]: true });
@@ -35,10 +33,10 @@ const Yellow = (props) => {
           {!state.showImage1 &&
             <div className="reveal1">
               <Button text="REVEAL" styles="circle" onClick={() => showImage(1)} />
+              <div className={`cover1 ${state.showImage1 ? "show" : ""}`} />
             </div>
           }
           <div className="circle1" />
-          <div className={`cover1 ${state.showImage1 ? "show" : ""}`} />
         </section>
 
         {/* Section Text */}
